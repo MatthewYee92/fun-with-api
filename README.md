7164a08aa2745f2f772345dabb4a68b4f637b1fe
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-11-07 16:19:49 -0800

    uploaded all new files for relay.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# fun-with-api
-Having some fun with the Poke API while learning relay
+<p align="center">
+  <img src="https://github.com/lucasbento/react-relay-pokemon/raw/master/content/logo.png">
+</p>
+
+<h1 align="center">React Relay Pokémon</h1>
+<p align="center">
+  Use React & Relay as your Pokedex!<br />
+  <a href="https://react-relay-pokemon.now.sh/">See the live demo</a>
+</p>
+
+### This is a project built to be used with [GraphQL Pokémon](https://github.com/lucasbento/graphql-pokemon)
+
+> Check out the [GraphiQL interface](https://graphql-pokemon.now.sh/) too!
+
+## Running
+
+> Copy `.env.example` file to create `.env` and `.env.local` which will be, respectively, the environment variables for production and local-running.
+
+### Production
+
+```sh
+  yarn
+  yarn run build # The files will be available on ./dist
+```
+
+### Development
+
+```sh
+  yarn
+  yarn run start
+```
+
+## Disclaimer
+
+This was built as part of a talk on Relay & GraphQL at [@ReactSP](https://meetup.com/pt-BR/ReactJS-SP) meetup, check us out, we build cool stuff. ;)
---
+++ src/components/Pokemon/PokemonDetail.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import Relay from 'react-relay';
+import { withRouter } from 'react-router';
+
+import PokemonEvolution from './PokemonEvolution';
+
+import { Card, CardActions, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import Chip from 'material-ui/Chip';
+import FlatButton from 'material-ui/FlatButton';
+
+class PokemonDetail extends Component {
+  render() {
+    const {
+      number,
+      name,
+      image,
+      types,
+      weaknesses,
+      evolutions,
+    } = this.props.query.pokemon;
+
+    return (
+      <Card>
+        <CardMedia
+          overlay={
+            <CardTitle
+              title={`${number} - ${name}`}
+              subtitle={types.join(', ')}
+            />
+          }
+          style={styles.cardMedia}
+        >
+          <img
+            src={image}
+            style={styles.pokemonImage}
+          />
+        </CardMedia>
+        <CardText>
+          <div style={styles.chips}>
+            {types.map((type, index) =>
+              <Chip
+                key={index}
+                style={styles.chip}
+              >
+                {type}
+              </Chip>
+            )}
+          </div>
+          <div style={styles.chips}>
+            {weaknesses.map((weakness, index) =>
+              <Chip
+                key={index}
+                style={styles.chip}
+              >
+                {weakness}
+              </Chip>
+            )}
+          </div>
+          <PokemonEvolution evolutions={evolutions|| null} />
+        </CardText>
+        <CardActions style={styles.cardActions}>
+          <FlatButton
+            label="Back"
+            onTouchTap={() => this.props.router.goBack()}
+          />
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+const styles = {
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  chips: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    margin: 5,
+  },
+  cardMedia: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  pokemonImage: {
+    minWidth: 200,
+    maxWidth: 200,
+  },
+};
+
+export default Relay.createContainer(withRouter(PokemonDetail), {
+  initialVariables: {
+    id: null,
+  },
+  fragments: {
+    query: () => Relay.QL`
+      fragment on Query {
+        pokemon(id: $id) {
+          number
+          name
+          image
+          types
+          weaknesses
+          evolutions {
+            ${PokemonEvolution.getFragment('evolutions')}
+          }
+        }
+      }
+    `,
+  },
+});
